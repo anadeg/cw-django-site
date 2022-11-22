242ae795799d14b01360327ad9fd721ddaeaa380
АД <degterevastasa@gmail.com>
2022-11-22 16:54:40 +0300

    added homepage

--- cw_site/settings.py
+++ cw_site/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'cw_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'htmls')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
