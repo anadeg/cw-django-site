a811e5849ef37a66bf3d444e378d4d856373c9cf
АД <degterevastasa@gmail.com>
2022-12-01 20:28:36 +0300

    added time measurement

--- advise_agent/advise.py
+++ advise_agent/advise.py
@@ -16,6 +16,8 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
 
+import time
+
 
 # DATA_FILE = "cw-data-2.csv"
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,30 +51,28 @@ class Advise:
         self.X = data.drop(['he'], axis=1)
         self.Y = data['he']
 
-        # self.X = self.features_engineering(self.X)
+        self.X = self.features_engineering(self.X)
 
     def features_engineering(self, x):
-        x['journ-int_com-theo'] = x['journ'] + x['int_com'] + x['theo']
+        x['journ-int_com'] = x['journ'] + x['int_com']
         x['lingo-teach-psy'] = x['lingo'] + x['teach'] + x['psy']
 
         x['it-math'] = x['it'] + x['math']
         x['it-design'] = x['it'] + x['design']
         x['math-physics'] = x['math'] + x['physics']
         x['phys_cult-tourism'] = x['phys_cult'] + x['turism']
-        x['ss-law'] = x['ss'] + x['law']
-        x['ss-psy'] = x['ss'] + x['psy']
-        x['ss-journ'] = x['ss'] + x['journ']
-        x['ss-lingo-hist'] = x['ss'] + x['lingo'] + x['hist']
+        x['ss-lingo-hist-journ'] = x['ss'] + x['lingo'] + x['hist'] + x['journ']
         x['phys_cult-bio'] = x['phys_cult'] + x['bio']
         x['chem-forest'] = x['chem'] + x['forest']
+        x['journ-ss-lingo'] = x['journ'] + x['ss'] + x['lingo']
 
-        x.drop(columns=['journ', 'int_com', 'theo',
+        x.drop(columns=['journ', 'int_com',
                         'lingo', 'teach', 'psy',
                         'it', 'math',
                         'design',
                         'physics',
                         'phys_cult', 'turism',
-                        'ss', 'law',
+                        'ss',
                         'hist',
                         'bio',
                         'chem', 'forest'],
@@ -99,7 +99,11 @@ class Advise:
             best_model = models[0]
             best = 0
             for model in models:
+                start = time.time()
                 cv_score = cross_val_score(model, self.X, self.Y, cv=3)
+                end = time.time()
+                delta = end - start
+                print(f'model {model} estimating takes {delta} seconds')
                 if best < cv_score.mean():
                     best = cv_score.mean()
                     best_model = model
@@ -136,7 +140,7 @@ def advise_he(to_predict, adv):
                "Минск": 5, "Минская": 6, "Могилевская": 7, "Могилёвская": 7, "Не важно": 0}
     to_predict['region'].replace(mapping, inplace=True)
     to_predict.fillna(0, inplace=True)
-    # predict = adv.features_engineering(to_predict)
+    predict = adv.features_engineering(to_predict)
 
     predict = np.array(to_predict)
     results = adv.advise(predict)
@@ -152,18 +156,18 @@ if __name__ == '__main__':
     svc = SVC()
     knc = KNeighborsClassifier()
     xgb = XGBClassifier()
-    cbc = CatBoostClassifier(verbose=False)
+    # cbc = CatBoostClassifier(verbose=False)
 
-    models = [lr, rfc, svc, knc, xgb, cbc]
-    # adv.teach(models, preferred_model=rfc)
-    adv.teach(models)
+    models = [lr, rfc, svc, knc, xgb]
+    adv.teach(models, preferred_model=rfc)
+    # adv.teach(models)
 
     predict = pd.read_csv('cw-pashka.csv')
     mapping = {"Брестская": 1, "Бресткая": 1, "Витебская": 2, "Гомельская": 3, "Гродненская": 4,
                "Минск": 5, "Минская": 6, "Могилевская": 7, "Могилёвская": 7}
     predict['region'].replace(mapping, inplace=True)
     predict.fillna(0, inplace=True)
-    # predict = adv.features_engineering(predict)
+    predict = adv.features_engineering(predict)
 
     print(f"Model \t\t\t|\t {adv.model}")
     y_true = adv.Y
