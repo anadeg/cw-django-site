53ff939b544984685b97f5e93125554234270058
АД <degterevastasa@gmail.com>
2022-12-01 20:28:11 +0300

    fixed eda

--- advise_agent/eda.py
+++ advise_agent/eda.py
@@ -10,17 +10,20 @@ reg_nan = data["region"].isna().sum()
 
 print(f'Region Data\nTotal Values \t|\t { data["region"].count() }\nMissing Values \t|\t {reg_nan}\n')
 
-sns.countplot(x=data['region'])
-plt.title('Region Distribution')
-plt.show()
+# sns.countplot(x=data['region'])
+# plt.title('Region Distribution')
+# plt.show()
 
 data['region'].fillna(data['region'].mode()[0], inplace=True)
 data.fillna(0, inplace=True)
 
-d_corr = data.corr()
-fig, ax = plt.subplots()
-ax = sns.heatmap(d_corr, cmap='YlGnBu')
-plt.show()
+# d_corr = data.corr()
+# d_corr = d_corr.where((d_corr < 0.3) & (d_corr >= 0.2))
+# fig, ax = plt.subplots()
+# ax = sns.heatmap(d_corr, cmap='YlGnBu')
+# plt.show()
+
+# print(d_corr.to_string())
 
 # >= 0.3
 # journ --- int_com, theo
@@ -32,24 +35,55 @@ plt.show()
 
 # 0.2 <= ... < 0.3
 # bio --- phys_cult
-# geo --- hist, int_com, theo, physics
+# geo --- hist
 # design --- it
-# journ --- ss
+# journ --- ss, lingo
 # it --- design, math
-# hist --- geo, lingo, teach, ss, physics
+# hist --- geo, lingo, teach, ss
 # forest --- chem
 # lingo --- journ, hist, ss
 # math --- it, physics
-# int_com --- geo,
+# int_com --- teo,
 # teach --- hist, physics
-# psy --- law
-# ss --- journ, hist, lingo, law
-# theo --- geo
-# physics --- geo, hist, math, teach
+# ss --- journ, hist, lingo, theo
+# theo --- int_com, ss
+# physics --- math, teach
 # turism --- phys_cult
 # phys_cult --- bio, turism
 # chem --- forest
-# law --- psy, ss
+
+def features_engineering(x):
+    x['journ-int_com'] = x['journ'] + x['int_com']
+    x['lingo-teach-psy'] = x['lingo'] + x['teach'] + x['psy']
+
+    x['it-math'] = x['it'] + x['math']
+    x['it-design'] = x['it'] + x['design']
+    x['math-physics'] = x['math'] + x['physics']
+    x['phys_cult-tourism'] = x['phys_cult'] + x['turism']
+    x['ss-lingo-hist-journ'] = x['ss'] + x['lingo'] + x['hist'] + x['journ']
+    x['phys_cult-bio'] = x['phys_cult'] + x['bio']
+    x['chem-forest'] = x['chem'] + x['forest']
+    x['journ-ss-lingo'] = x['journ'] + x['ss'] + x['lingo']
+
+    x.drop(columns=['journ', 'int_com',
+                    'lingo', 'teach', 'psy',
+                    'it', 'math',
+                    'design',
+                    'physics',
+                    'phys_cult', 'turism',
+                    'ss',
+                    'hist',
+                    'bio',
+                    'chem', 'forest'],
+           inplace=True)
+
+features_engineering(data)
+
+d_corr = data.corr()
+# d_corr = d_corr.where((d_corr < 0.3) & (d_corr >= 0.2))
+fig, ax = plt.subplots()
+ax = sns.heatmap(d_corr, cmap='YlGnBu')
+plt.show()
 
 
 
